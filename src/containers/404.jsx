7f1cff3be985e69aea58153a11109dc7bf746b77
T0ast <m.briedl@live.at>
2018-06-18 15:56:07 +0200

    Added more information to 404 page
    It now shows the faulty route path as well as a more descriptive error message.

--- src/containers/404.jsx
+++ src/containers/404.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { withSiteData } from 'react-static'
 //
 import StandardPage from '../pages/StandardPage'
 
-export default () => (
+export default withSiteData(({ match, location, history }) => (
 	<StandardPage>
 		<div>
 			<h1>Status 404</h1>
-			<p>Nop, not there</p>
+			<code>{location.pathname}</code>
+			<p>This route doesn't exist</p>
 		</div>
 	</StandardPage>
-)
+))
