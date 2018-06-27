a1b4f096b7e10267eb5946ea91911ecaa4ca7209
T0ast <m.briedl@live.at>
2018-06-27 23:53:23 +0200

    Added empty list placeholder

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 //
 import '../scss/components/lists.scss'
+import placeholder from '../media/images/unused_character_face.png'
 
-const List = ({ id, className, children }) => (
-    <ul className={"list " + (className||"")}>
+const List = ({ id, className, children }) => {
+    if(children && children.length > 0) return listWithElements(className, children)
+    return emptyList(id, className)
+}
+
+const listWithElements = (id, className, children) => (
+    <ul className={"list " + (className||"")} id={id}>
         {children}
     </ul>
+)
+
+const emptyList = (id, className) => (
+    <div className={"empty-list " + (className||"")} id={id}>
+        <div className="empty-list-placeholder">
+            <img src={placeholder} alt={"Unused Character from Kill Me Baby"}/>
+            <span>This list is empty</span>
+        </div>
+    </div>
 )
  
 export default List
