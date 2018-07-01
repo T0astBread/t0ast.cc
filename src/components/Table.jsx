6269fb9109a1f79a02d2cb2375835844289dcc6d
T0ast <m.briedl@live.at>
2018-07-01 03:12:20 +0200

    Improved table behaviour on small screens
    On screens that are small but not small enough to trigger the mobile layout, the table is now horizontally scrollable (and the borders are sticky to the left and right side, so it doesn't look ugly).

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 
 const Table = ({ headings, rows }) => (
-    <table>
-        <thead>
-            <tr>
-                {headings.map(heading => <th>{heading}</th>)}
-            </tr>
-        </thead>
-        <tbody>
-            {rows.map(row => {
-                let i = 0
-                return (
-                    <tr>
-                        {row.map(cell => (
-                            <td>
-                                <div className="responsive-cell-heading">{headings[i++]}</div>
-                                <div className="cell-content">
-                                    {cell}
-                                </div>
-                            </td>
-                        ))}
-                    </tr>
-                )
-            })}
-        </tbody>
-    </table>
+    <div className="table-wrapper">
+        <div className="static-table-border"></div>
+        <table>
+            <thead>
+                <tr>
+                    {headings.map(heading => <th>{heading}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map(row => {
+                    let i = 0
+                    return (
+                        <tr>
+                            {row.map(cell => (
+                                <td>
+                                    <div className="responsive-cell-heading">{headings[i++]}</div>
+                                    <div className="cell-content">
+                                        {cell}
+                                    </div>
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    </div>
 )
 
 export default Table
