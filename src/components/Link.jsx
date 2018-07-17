7713f720e23314d108d0cbb1f6bcd9c196bb2dbb
T0ast <t0astbreaaad@gmail.com>
2018-07-17 07:24:26 +0200

    Moved local link detection to Link component & improved it

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Link } from 'react-static'
 
+const hosts = ["t0ast.cc", "t0ast.netlify.com"]
+const linkIsOnHost = link => link && (linkPointsToLocalRoute(link) || urlWithoutProtocolIsOnHost(link))
+const linkPointsToLocalRoute = link => !(link.startsWith("http://") || link.startsWith("https://"))
+const urlIsOnHost = url => urlWithoutProtocolIsOnHost(stripProtocolFromUrl(url))
+const stripProtocolFromUrl = url => url.replace(/^http(s)?:\/\//, "")
+const urlWithoutProtocolIsOnHost = url => hosts.some(host => url.startsWith(host))
+
 const EXTERNAL_CLASS_NAME = "external"
 
 const LinkX = props => {
@@ -9,6 +16,9 @@ const LinkX = props => {
             `${props.className.toString()} ${EXTERNAL_CLASS_NAME}` :
             EXTERNAL_CLASS_NAME
         props = {...props, className: newClassName}
+    }
+    if(props.newtab == undefined) {
+        props = {...props, newtab: !linkIsOnHost(props.to)}
     }
     if(props.newtab) {
         props = {...props, target: "_blank", rel: "noopener norefferrer"}
--- src/components/ShelfItem.jsx
+++ src/components/ShelfItem.jsx
@@ -3,15 +3,11 @@ import React from 'react'
 import "../scss/components/shelf.scss"
 import Link from "./Link"
 
-const hosts = ["t0ast.cc", "t0ast.netlify.com"]
-const urlWithoutProtocolIsOnHost = url => hosts.some(host => url.startsWith(host))
-
 const ShelfItem = ({ name, website, description, language }) => {
-    const urlIsLocal = website && urlWithoutProtocolIsOnHost(website.replace(/^http(s)?:\/\//, ""))
     return (
         <div className="shelf-item">
             <h5>
-                <Link to={website} newtab={!urlIsLocal}>{name}</Link>
+                <Link to={website}>{name}</Link>
             </h5>
             <p>{description}</p>
             <div className="language-display">
