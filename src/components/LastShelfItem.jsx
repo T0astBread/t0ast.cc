928b82f98a8cef511764ad03d973c042c5000e2d
T0astBread <t0astbreaaad@gmail.com>
2018-07-21 21:14:30 +0200

    Gave links in LastShelfItem hrefs
    Except for the RSS/Atom link, since that's not implemented yet

--- src/components/LastShelfItem.jsx
+++ src/components/LastShelfItem.jsx
@@ -4,7 +4,7 @@ import Link from './Link';
 const LastShelfItem = () => (
     <div className="shelf-item last-shelf-item">
         <h5>Stay tuned for more to come!</h5>
-        <span><Link>RSS</Link> | <Link>GitHub</Link> | <Link>Twitter</Link></span>
+        <span><Link>RSS</Link> | <Link to="https://github.com/t0astbread">GitHub</Link> | <Link to="https://twitter.com/t0astbread">Twitter</Link></span>
     </div>
 )
  
