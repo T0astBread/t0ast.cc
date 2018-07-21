1a21f40c6b1ff156d838f6af67369915b1e10f0d
T0astBread <t0astbreaaad@gmail.com>
2018-07-21 22:38:41 +0200

    Improved text on about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -8,12 +8,17 @@ export default () => (
 		<h1>About</h1>
 		<section>
 			<h2>About Me</h2>
-			<p>No one needs an about page about me</p>
+			<p>Err, no I don't wanna write that right now.</p>
 		</section>
 		<section>
 			<h2>About this Site</h2>
 			<p>
-				This site is written in JavaScript (JSX) using <Link to="https://react-static.js.org" external newtab>React Static</Link>.
+				This site is written in JavaScript (JSX) using <Link to="https://react-static.js.org">React Static</Link>.
+				It follows the principles fo the <Link to="https://jamstack.org">JAMstack</Link> and is hosted on <Link to="https://netlify.com">Netlify</Link>.
+			</p>
+			<p>
+				Note that it's still in development, so it's a little rough around the edges.
+				You can follow its progress and contribute on <Link to="https://github.com/t0astbread/t0ast.cc">GitHub</Link>.
 			</p>
 		</section>
 	</StandardPage>
